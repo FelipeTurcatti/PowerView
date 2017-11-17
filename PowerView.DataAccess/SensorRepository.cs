eadc26def64d6f5978f66d7ac2e382d5ccd0fc77
Lisandro Schneir <lisandrosc@hotmail.com>
2017-11-17 09:24:55 -0300

    Add Sensor Controllers/SensorController

---
+++ PowerView.DataAccess/SensorRepository.cs
@@ -0,0 +1,31 @@
+﻿using PowerView.DataAccess.Abstractions;
+using PowerView.Domain;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PowerView.DataAccess
+{
+    public class SensorRepository : GenericEntityFrameworkRepository<Sensor>, ISensorRepository
+    {
+        #region Constructor
+        public SensorRepository(PowerViewDbContext context) : base(context)
+        {
+        }
+        #endregion
+
+        #region Methods
+        public IList<Sensor> Get()
+        {
+            return this.Get().ToList();
+        }
+
+        public Sensor Get(int sensorID)
+        {
+            return this.Get(x => x.SensorId==sensorID).FirstOrDefault();
+        }
+        #endregion
+    }
+}
---
+++ PowerView.Domain.Logic/Interface/ISensorService.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PowerView.Domain.Logic.Interface
+{
+    public interface ISensorService
+    {
+        Sensor GetSensor(Int32 sensorID);
+
+        IList<Sensor> GetSensors();
+    }
+}
