34cb5b04c8b7584de6ea6b032426ef8e8c2068ee
Felipe Turcatti <felipe_turcatti@transcanada.com>
2017-11-17 02:28:42 -0300

    Configuring Owin as a middleware for Web Api project. Install and configure Swagger as the documentation framework

---
+++ PowerView.Web.Api/App_Start/SwaggerConfig.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Http;
+using Swashbuckle.Application;
+
+namespace PowerView.Web.Api
+{
+    public class SwaggerConfig
+    {
+        public static void Register(HttpConfiguration config)
+        {
+            config.EnableSwagger(c =>
+            {
+                c.SingleApiVersion("v1", "PowerView API")
+                .Description("Power view Web Api services")
+                .Contact(cc => cc.Name("Lisandro \"El Lechón\" Schneir")
+                .Url("https://github.com/FelipeTurcatti/PowerView")
+                .Email("lisandro.schneir@gmail.com"))
+                .License(lc => lc.Name("License")
+                .Url("https://github.com/FelipeTurcatti/PowerView"));
+            }).EnableSwaggerUi();
+
+        }
+    }
+}
