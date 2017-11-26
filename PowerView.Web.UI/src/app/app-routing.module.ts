7e5967460064c21664e9234240f0aeea9324c155
Felipe Turcatti <felipe_turcatti@transcanada.com>
2017-11-25 21:47:42 -0300

    Integrate Angular js into MVC project. Start a Sensor admin view

---
+++ PowerView.Web.UI/src/app/app-routing.module.ts
@@ -0,0 +1,22 @@
+﻿import { NgModule } from '@angular/core';
+import { RouterModule, Routes } from '@angular/router';
+
+import { SensorComponent } from './sensor/sensor.component';
+
+const routes: Routes = [
+    {
+        path: 'sensor',
+        component: SensorComponent
+    },
+    {
+        path: 'SensorAdmin/Index',
+        redirectTo: 'sensor'
+    }
+];
+
+@NgModule({
+    imports: [RouterModule.forRoot(routes)],
+    exports: [RouterModule]
+})
+
+export class AppRoutingModule { }
---
+++ PowerView.Web.UI/src/app/sensor/sensor.component.ts
@@ -0,0 +1,7 @@
+﻿import { Component } from "@angular/core";
+
+@Component({
+    templateUrl: "./sensor.component.html"
+})
+
+export class SensorComponent { }
