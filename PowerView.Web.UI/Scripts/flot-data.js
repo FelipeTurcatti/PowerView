493fa339e0f4ca3594c68cfe2b7edaa14386246f
Felipe Turcatti <felipe_turcatti@transcanada.com>
2017-11-27 17:03:13 -0300

    Fix build

--- PowerView.Web.UI/Scripts/flot-data.js
+++ PowerView.Web.UI/Scripts/flot-data.js
@@ -1100,7 +1100,6 @@ $(function() {
 });
 
 //Flot Moving Line Chart
-
 $(function() {
 
     var container = $("#flot-line-chart-moving");
@@ -1200,7 +1199,6 @@ $(function() {
 });
 
 //Flot Bar Chart
-
 $(function() {
 
     var barOptions = {
