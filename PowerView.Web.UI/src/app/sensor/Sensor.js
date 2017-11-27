a55f3f0ac805b3afc06936d220e07884b1ffd3fa
Felipe Turcatti <felipe_turcatti@transcanada.com>
2017-11-27 15:24:54 -0300

    remove angular 2 from UI project since we will work with raw MVC views

--- PowerView.Web.UI/src/app/sensor/Sensor.js
+++
@@ -1,9 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-var Sensor = (function () {
-    function Sensor() {
-    }
-    return Sensor;
-}());
-exports.Sensor = Sensor;
-//# sourceMappingURL=Sensor.js.map
--- PowerView.Web.UI/src/app/sensor/sensor.ts
+++
@@ -1,7 +0,0 @@
-﻿export class Sensor {
-    sensorId: number;
-    name: string;
-    description: string;
-    location: string;
-    note: string;
-}
