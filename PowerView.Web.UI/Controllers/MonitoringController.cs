eab31c2767a02e7a168e9fdf67b7f8d033a0adab
Felipe Turcatti <felipe_turcatti@transcanada.com>
2017-12-02 18:57:45 -0300

    Fixed some missing files in Bower folder

---
+++ PowerView.Web.UI/Controllers/MonitoringController.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace PowerView.Web.UI.Controllers
+{
+    /// <summary>
+    /// Manage System's metric controllers
+    /// </summary>
+    public class MonitoringController : Controller
+    {
+        /// <summary>
+        /// MVC Main view.-
+        /// </summary>
+        /// <returns></returns>
+        // GET: Monitoring
+        public ActionResult Index()
+        {
+            return View();
+        }
+    }
+}
